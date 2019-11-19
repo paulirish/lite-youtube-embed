cfe24b4bd7fa9d4e89358c093b43dc4e688ca1e7
Paul Welsh <spacedawwwg@gmail.com>
2019-11-19 13:25:09 +0000

    feat: add youtube icon as SVG

--- src/lite-yt-embed.js
+++ src/lite-yt-embed.js
@@ -17,7 +17,7 @@ class LiteYTEmbed extends HTMLElement {
         // Gotta encode the untrusted values
         // https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#rule-2---attribute-escape-before-inserting-untrusted-data-into-html-common-attributes
         this.videoId = encodeURIComponent(this.getAttribute('videoid'));
-        this.playLabel = encodeURIComponent(this.getAttribute('playlabel'));
+        this.playLabel = this.getAttribute('playlabel') ? encodeURIComponent(this.getAttribute('playlabel')) : null;
 
         /**
          * Lo, the youtube placeholder image!  (aka the thumbnail, poster image, etc)
@@ -51,6 +51,14 @@ class LiteYTEmbed extends HTMLElement {
             playBtn.type = 'button';
             playBtn.title = this.playLabel ? decodeURIComponent(this.playLabel) : `Play Video: ${this.videoId}`;
             playBtn.classList.add('lty-playbtn');
+            const iconHTML = `
+                <svg class="lty-playicon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 68 48">
+                    <defs/>
+                    <path class="lty-playicon-base" d="M66.52 7.74c-.78-2.93-2.49-5.41-5.42-6.19C55.79.13 34 0 34 0S12.21.13 6.9 1.55c-2.93.78-4.63 3.26-5.42 6.19C.06 13.05 0 24 0 24s.06 10.95 1.48 16.26c.78 2.93 2.49 5.41 5.42 6.19C12.21 47.87 34 48 34 48s21.79-.13 27.1-1.55c2.93-.78 4.64-3.26 5.42-6.19C67.94 34.95 68 24 68 24s-.06-10.95-1.48-16.26z" class="ytp-large-play-button-bg"/>
+                    <path class="lty-playicon-play" d="M45 24L27 14v20"/>
+                </svg>
+            `;
+            playBtn.insertAdjacentHTML('afterbegin', iconHTML);
             this.append(playBtn);
         }
 
