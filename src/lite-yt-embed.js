4af3e0d686651427ebb148615b496c5d274bd1eb
Paul Welsh <spacedawwwg@gmail.com>
2020-01-06 10:09:59 +0000

    feat: add accessible play button

--- src/lite-yt-embed.js
+++ src/lite-yt-embed.js
@@ -17,6 +17,7 @@ class LiteYTEmbed extends HTMLElement {
         // Gotta encode the untrusted value
         // https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#rule-2---attribute-escape-before-inserting-untrusted-data-into-html-common-attributes
         this.videoId = encodeURIComponent(this.getAttribute('videoid'));
+        this.playLabel = this.getAttribute('playlabel') ? encodeURIComponent(this.getAttribute('playlabel')) : 'Play';
 
         /**
          * Lo, the youtube placeholder image!  (aka the thumbnail, poster image, etc)
@@ -43,9 +44,15 @@ class LiteYTEmbed extends HTMLElement {
     connectedCallback() {
         this.style.backgroundImage = `url("${this.posterUrl}")`;
 
-        const playBtn = document.createElement('div');
-        playBtn.classList.add('lty-playbtn');
-        this.append(playBtn);
+        let playBtn = this.querySelector('.lty-playbtn');
+
+        if (!playBtn) {
+            playBtn = document.createElement('button');
+            playBtn.type = 'button';
+            playBtn.classList.add('lty-playbtn');
+            playBtn.title = decodeURIComponent(this.playLabel);
+            this.append(playBtn);
+        }
 
         // On hover (or tap), warm up the TCP connections we're (likely) about to use.
         this.addEventListener('pointerover', LiteYTEmbed.warmConnections, {once: true});
