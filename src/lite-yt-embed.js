0fd4caf2dc2545898d2505d13245fb9a7a05bb0f
frankie-tech <26461046+frankie-tech@users.noreply.github.com>
2020-10-28 13:49:54 -0400

    checks the style attribute for a posterimage
    new static method to interpret the background image url()
    into an actual url/path

--- src/lite-yt-embed.js
+++ src/lite-yt-embed.js
@@ -10,120 +10,141 @@
  *   https://github.com/Daugilas/lazyYT
  *   https://github.com/vb/lazyframe
  */
-class LiteYTEmbed extends HTMLElement {
-	connectedCallback() {
-		// Gotta encode the untrusted value
-		// https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#rule-2---attribute-escape-before-inserting-untrusted-data-into-html-common-attributes
-		this.videoId = encodeURIComponent(this.getAttribute('videoid'));
-		const playLabel = this.getAttribute('playlabel');
-		this.playLabel = playLabel ? encodeURIComponent(playLabel) : 'Play';
+
+// Register custom element
+customElements.define(
+	'lite-youtube',
+	class LiteYTEmbed extends HTMLElement {
+		connectedCallback() {
+			// Gotta encode the untrusted value
+			// https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#rule-2---attribute-escape-before-inserting-untrusted-data-into-html-common-attributes
+			this.videoId = encodeURIComponent(this.getAttribute('videoid'));
+			const playLabel = this.getAttribute('playlabel');
+			this.playLabel = playLabel ? encodeURIComponent(playLabel) : 'Play';
+
+			/**
+			 * Lo, the youtube placeholder image!  (aka the thumbnail, poster image, etc)
+			 * There is much internet debate on the reliability of thumbnail URLs. Weak consensus is that you
+			 * cannot rely on anything and have to use the YouTube Data API.
+			 *
+			 * amp-youtube also eschews using the API, so they just try sddefault with a hqdefault fallback:
+			 *   https://github.com/ampproject/amphtml/blob/6039a6317325a8589586e72e4f98c047dbcbf7ba/extensions/amp-youtube/0.1/amp-youtube.js#L498-L537
+			 * For now I'm gonna go with this confident (lol) assertion: https://stackoverflow.com/a/20542029, though I'll use `i.ytimg` to optimize for origin reuse.
+			 *
+			 * Worth noting that sddefault is _higher_ resolution than hqdefault. Naming is hard. ;)
+			 * From my own testing, it appears that hqdefault is ALWAYS there sddefault is missing for ~10% of videos
+			 *
+			 * TODO: Do the sddefault->hqdefault fallback
+			 *       - When doing this, apply referrerpolicy (https://github.com/ampproject/amphtml/pull/3940)
+			 * TODO: Consider using webp if supported, falling back to jpg
+			 */
+			const posterImage = this.getAttribute('posterimage');
+			const backgroundImage = this.style.backgroundImage;
+			this.posterUrl = posterImage
+				? posterImage
+				: backgroundImage !== ''
+				? LiteYTEmbed.getBackgroundImage(backgroundImage)
+				: `https://i.ytimg.com/vi/${this.videoId}/hqdefault.jpg`;
+			// Warm the connection for the poster image
+			LiteYTEmbed.addPrefetch('preload', this.posterUrl, 'image');
+
+			this.style.backgroundImage = `url("${this.posterUrl}")`;
+
+			let playBtn = this.querySelector('.lty-playbtn');
+
+			if (!playBtn) {
+				playBtn = document.createElement('button');
+				playBtn.type = 'button';
+				playBtn.classList.add('lty-playbtn');
+				playBtn.title = decodeURIComponent(this.playLabel);
+				this.append(playBtn);
+			}
+
+			// On hover (or tap), warm up the TCP connections we're (likely) about to use.
+			this.addEventListener('pointerover', LiteYTEmbed.warmConnections, {
+				once: true,
+			});
+
+			// Once the user clicks, add the real iframe and drop our play button
+			// TODO: In the future we could be like amp-youtube and silently swap in the iframe during idle time
+			//   We'd want to only do this for in-viewport or near-viewport ones: https://github.com/ampproject/amphtml/pull/5003
+			this.addEventListener('click', (e) => this.addIframe());
+		}
+
+		// // TODO: Support the the user changing the [videoid] attribute
+		// attributeChangedCallback() {
+		// }
 
 		/**
-		 * Lo, the youtube placeholder image!  (aka the thumbnail, poster image, etc)
-		 * There is much internet debate on the reliability of thumbnail URLs. Weak consensus is that you
-		 * cannot rely on anything and have to use the YouTube Data API.
-		 *
-		 * amp-youtube also eschews using the API, so they just try sddefault with a hqdefault fallback:
-		 *   https://github.com/ampproject/amphtml/blob/6039a6317325a8589586e72e4f98c047dbcbf7ba/extensions/amp-youtube/0.1/amp-youtube.js#L498-L537
-		 * For now I'm gonna go with this confident (lol) assertion: https://stackoverflow.com/a/20542029, though I'll use `i.ytimg` to optimize for origin reuse.
-		 *
-		 * Worth noting that sddefault is _higher_ resolution than hqdefault. Naming is hard. ;)
-		 * From my own testing, it appears that hqdefault is ALWAYS there sddefault is missing for ~10% of videos
-		 *
-		 * TODO: Do the sddefault->hqdefault fallback
-		 *       - When doing this, apply referrerpolicy (https://github.com/ampproject/amphtml/pull/3940)
-		 * TODO: Consider using webp if supported, falling back to jpg
+		 * Add a <link rel={preload | preconnect} ...> to the head
 		 */
-		const posterImage = this.getAttribute('posterimage');
-		this.posterUrl = posterImage
-			? posterImage
-			: `https://i.ytimg.com/vi/${this.videoId}/hqdefault.jpg`;
-		// Warm the connection for the poster image
-		LiteYTEmbed.addPrefetch('preload', this.posterUrl, 'image');
-
-		this.style.backgroundImage = `url("${this.posterUrl}")`;
-
-		let playBtn = this.querySelector('.lty-playbtn');
-
-		if (!playBtn) {
-			playBtn = document.createElement('button');
-			playBtn.type = 'button';
-			playBtn.classList.add('lty-playbtn');
-			playBtn.title = decodeURIComponent(this.playLabel);
-			this.append(playBtn);
+		static addPrefetch(kind, url, as) {
+			const linkElem = document.createElement('link');
+			linkElem.rel = kind;
+			linkElem.href = url;
+			if (as) {
+				linkElem.as = as;
+			}
+			document.head.append(linkElem);
 		}
 
-		// On hover (or tap), warm up the TCP connections we're (likely) about to use.
-		this.addEventListener('pointerover', LiteYTEmbed.warmConnections, {
-			once: true,
-		});
+		/**
+		 * Begin pre-connecting to warm up the iframe load
+		 * Since the embed's network requests load within its iframe,
+		 *   preload/prefetch'ing them outside the iframe will only cause double-downloads.
+		 * So, the best we can do is warm up a few connections to origins that are in the critical path.
+		 *
+		 * Maybe `<link rel=preload as=document>` would work, but it's unsupported: http://crbug.com/593267
+		 * But TBH, I don't think it'll happen soon with Site Isolation and split caches adding serious complexity.
+		 */
+		static warmConnections() {
+			if (LiteYTEmbed.preconnected) return;
 
-		// Once the user clicks, add the real iframe and drop our play button
-		// TODO: In the future we could be like amp-youtube and silently swap in the iframe during idle time
-		//   We'd want to only do this for in-viewport or near-viewport ones: https://github.com/ampproject/amphtml/pull/5003
-		this.addEventListener('click', (e) => this.addIframe());
-	}
+			// The iframe document and most of its subresources come right off youtube.com
+			LiteYTEmbed.addPrefetch(
+				'preconnect',
+				'https://www.youtube-nocookie.com',
+			);
+			// The botguard script is fetched off from google.com
+			LiteYTEmbed.addPrefetch('preconnect', 'https://www.google.com');
 
-	// // TODO: Support the the user changing the [videoid] attribute
-	// attributeChangedCallback() {
-	// }
+			// Not certain if these ad related domains are in the critical path. Could verify with domain-specific throttling.
+			LiteYTEmbed.addPrefetch(
+				'preconnect',
+				'https://googleads.g.doubleclick.net',
+			);
+			LiteYTEmbed.addPrefetch(
+				'preconnect',
+				'https://static.doubleclick.net',
+			);
 
-	/**
-	 * Add a <link rel={preload | preconnect} ...> to the head
-	 */
-	static addPrefetch(kind, url, as) {
-		const linkElem = document.createElement('link');
-		linkElem.rel = kind;
-		linkElem.href = url;
-		if (as) {
-			linkElem.as = as;
+			LiteYTEmbed.preconnected = true;
 		}
-		document.head.append(linkElem);
-	}
 
-	/**
-	 * Begin pre-connecting to warm up the iframe load
-	 * Since the embed's network requests load within its iframe,
-	 *   preload/prefetch'ing them outside the iframe will only cause double-downloads.
-	 * So, the best we can do is warm up a few connections to origins that are in the critical path.
-	 *
-	 * Maybe `<link rel=preload as=document>` would work, but it's unsupported: http://crbug.com/593267
-	 * But TBH, I don't think it'll happen soon with Site Isolation and split caches adding serious complexity.
-	 */
-	static warmConnections() {
-		if (LiteYTEmbed.preconnected) return;
+		static getBackgroundImage(backgroundImage) {
+			const [match] = backgroundImage.match(/url(("|')?.*("|'))\)/g);
+			const isQuotes = match.indexOf('"') > 0;
+			const url = isQuotes
+				? match.slice(match.indexOf('"') + 1, -2)
+				: match.slice(match.indexOf("'") + 1, -2);
 
-		// The iframe document and most of its subresources come right off youtube.com
-		LiteYTEmbed.addPrefetch(
-			'preconnect',
-			'https://www.youtube-nocookie.com',
-		);
-		// The botguard script is fetched off from google.com
-		LiteYTEmbed.addPrefetch('preconnect', 'https://www.google.com');
+			return url;
+		}
 
-		// Not certain if these ad related domains are in the critical path. Could verify with domain-specific throttling.
-		LiteYTEmbed.addPrefetch(
-			'preconnect',
-			'https://googleads.g.doubleclick.net',
-		);
-		LiteYTEmbed.addPrefetch('preconnect', 'https://static.doubleclick.net');
-
-		LiteYTEmbed.preconnected = true;
-	}
-
-	addIframe() {
-		const params = new URLSearchParams(this.getAttribute('params') || []);
-		params.append('autoplay', '1');
-		const iframeHTML = `
+		addIframe() {
+			const params = new URLSearchParams(
+				this.getAttribute('params') || [],
+			);
+			params.append('autoplay', '1');
+			const iframeHTML = `
 <iframe width="560" height="315" frameborder="0"
   allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen
   src="https://www.youtube-nocookie.com/embed/${
 		this.videoId
   }?${params.toString()}"
 ></iframe>`;
-		this.insertAdjacentHTML('beforeend', iframeHTML);
-		this.classList.add('lyt-activated');
-	}
-}
-// Register custome element
-customElements.define('lite-youtube', LiteYTEmbed);
+			this.insertAdjacentHTML('beforeend', iframeHTML);
+			this.classList.add('lyt-activated');
+		}
+	},
+);
